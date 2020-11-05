e19426990cbaa867a453b3bfbfa762a85fd64e7c
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-05 21:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   const factsWithExclamationPoints = [] 
   
-  while (condition) {
+  while (facts[i]) {
   // code block to be executed
   }
   
