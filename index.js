ce643c61ace7f2219af697901e58bcd5bf381f13
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-05 20:48:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 function theBeatlesPlay(musicians, instruments) {
   const arr = []
   
-  for (let i=0; l = musicians.length; i<l; i++ )
+  for (let i=0; l = musicians.length; i<l; i++) {
+    
+  }
 }
