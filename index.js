37fa2fbc4cf8dbfc1769146f277bdf5bf600998f
Alena Trushnikova <alena.yu.t@gmail.com>
2020-11-05 20:55:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 function theBeatlesPlay(musicians, instruments) {
   const arr = []
   
-  for (let i=0, l = musicians.length; i < l; i++) {
-    arr.push('${musicians[i]} plays ${instruments[i]}')
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return arr
 }
